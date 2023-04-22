abb8cca86dfe6f53ce1e91e852c77ea2738ef668
ruityde <82042207+ruityde@users.noreply.github.com>
2023-04-22 20:22:50 +0200

    Add files via upload

--- pearl/src/io.jl
+++ pearl/src/io.jl
@@ -19,32 +19,48 @@ function readInputFile(inputFile::String)
     close(datafile)
 
     size_w = findnext(x -> x == "", data, 3) - 3
-    white_list = zeros(Int64, size_w, 2)
+    white_list = zeros(Int64, size_w)
     size_b = length(data) - size_w - 3
-    black_list = zeros(Int64, size_b, 2)
+    black_list = zeros(Int64, size_b)
 
     size_grid = parse(Int64, data[1])
 
     for i in 1:size_w
-        white_list[i, :] = parse.(Int64, split(data[i+2], ","))
+        w = parse.(Int64, split(data[i+2], ","))
+        white_list[i] = size_grid*(w[1]-1) + w[2]
     end
 
     for j in 1:size_b
-        black_list[j, :] = parse.(Int64, split(data[j+3+size_w], ","))
+        b = parse.(Int64, split(data[j+3+size_w], ","))
+        black_list[j] = size_grid*(b[1]-1) + b[2]
     end
 
     return size_grid, white_list, black_list
 
 end
 
-function displayGrid(size_grid::Int64, white_list::Matrix{Int64}, black_list::Matrix{Int64}, x::Vector{Tuple{Int64,Int64,Int64}})
+function displayGrid(size_grid::Int64, w::Vector{Int64}, b::Vector{Int64}, x::Vector{Tuple{Int64,Int64,Int64}})
     
     grid = zeros(Int64, size_grid, size_grid)
 
-    #for l in 1:n^2
+    #for l in 1:size_grid^2
         #x[l] = (l,2,3)
         #x[l] = (l,0,0)
     #end
+
+    white_list = zeros(Int64, length(w), 2)
+    black_list = zeros(Int64, length(b), 2)
+
+    for i in 1:length(w)
+        white_list[i,1] = floor(w[i]/size_grid)+1
+        white_list[i,2] = w[i]%size_grid
+    end
+
+    for j in 1: length(b)
+        black_list[j,1] = floor(b[j]/size_grid) +1
+        black_list[j,2] = b[j]%size_grid
+    end
+
 
     for i in 1:size(white_list,1)
         grid[white_list[i,1], white_list[i,2]] = 1
